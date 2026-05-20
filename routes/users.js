7d24e648e150537529e93a286f8dd3cc0794b646
umarJ-max <digitalcreatoruj@gmail.com>
2026-05-20 16:04:38 +0500

    fix users GET route accidentally deleted

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,17 @@ const User = require('../models/User');
 const auth = require('../middleware/auth');
 
 // Get all users except self
+router.get('/', auth, async (req, res) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.user.id } })
+      .select('-password');
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Update user name
 router.put('/update', auth, async (req, res) => {
   try {
     const { name } = req.body;
