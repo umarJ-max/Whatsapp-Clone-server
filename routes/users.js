dda1ce5934e842516945b25e5737af163ca4c711
umarJ-max <digitalcreatoruj@gmail.com>
2026-05-20 09:57:43 +0500

    add update user name route

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ const User = require('../models/User');
 const auth = require('../middleware/auth');
 
 // Get all users except self
-router.get('/', auth, async (req, res) => {
+router.put('/update', auth, async (req, res) => {
   try {
-    const users = await User.find({ _id: { $ne: req.user.id } })
-      .select('-password');
-    res.json(users);
+    const { name } = req.body;
+    await User.findByIdAndUpdate(req.user.id, { name });
+    res.json({ message: 'Updated' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
