459dd9a99704518b37373bdf188ca382998f1279
umarJ-max <digitalcreatoruj@gmail.com>
2026-05-25 08:17:05 +0500

    add admin and description to Chat model

--- models/Chat.js
+++ models/Chat.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
   isGroup: { type: Boolean, default: false },
   name: { type: String, default: '' },
+  description: { type: String, default: '' },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
   lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message', default: null },
 }, { timestamps: true });
 
